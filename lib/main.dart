c25506aa94fb3dc3c3044a883b2863fbedbb29eb
Ahmed Hussein <ahmed0zakarea@gmail.com>
2021-04-03 14:29:08 +0300

    initial

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:e_commerce/home_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return HomeScreen();
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      body: Center(
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.black,
+        ),
+      ),
+    );
+  }
+}
